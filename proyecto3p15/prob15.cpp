ecd2848418240ce5d44a6e23334d1176188ce635
zhilu <zhilu2004@alumnes.ilerna.com>
2025-09-17 13:09:54 +0200

    15

---
+++ proyecto3p15/prob15.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int x;
+
+	float rest ,rest2;
+	cout << "INTRO EL VALOR E: ";
+	cin >> x;
+
+	rest = 2.0 /( x+4);
+	rest2 = (1.0 + 4 - x) / (3*(x+3));
+
+	
+	cout << "EL RESULTADO DE LA PRIMERA EXPRESION ES: " << rest << endl;
+	cout << "EL RESULTADO DE LA SECONDA EXPRESION ES: " << rest2 << endl;
+
+
+}
+
+
